c4c4d6413580a7d43e57ad546370f8009711e1b8
Mark Brown (macbookpro) <mark.aleyn@gmail.com>
2024-03-29 11:08:14 +0000

    Initial Commit

---
+++ ApplicationTracker/ApplicationTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplicationTrackerApp.swift
+//  ApplicationTracker
+//
+//  Created by Mark Brown on 29/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ApplicationTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApplicationTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ApplicationTracker
+//
+//  Created by Mark Brown on 29/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
